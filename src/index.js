5355a4668913416b8166c242eb9189729c8108f5
Alina Kornilova <alinakornilova78@gmail.com>
2020-04-05 20:20:24 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,3 +2,5 @@ const addBtn = document.querySelector('#new-toy-btn')
 const toyForm = document.querySelector('.container')
 let addToy = false;
 let divCollect = document.querySelector('#toy-collection')
+
+function getToys(){
